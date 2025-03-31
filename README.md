911802e961d7ec7d53c743b55e4aefe9e4757730
Marcos_Montenegro <marcosmontenegro434@gmail.com>
2025-03-31 13:50:51 -0300

    Inicialización

---
+++ README.md
@@ -0,0 +1,13 @@
+# Introducción
+
+Para poder correr las conexiones se deben rellenar las credenciales de .env y copiar la private key, device certificate and root CA creadas en la pagina de IoT resoruces.
+
+## VENV y Requirements
+
+Para activar el entorno virtual debes efectuar un **comando en la terminal**.
+
+`venv\Scripts\activate`
+
+Y luego efectuamos otro comando para instalar las librerias basicas. Suponiendo que nos encontramos justo en la PATH donde esta nuestro proyecto, el comando sera...
+
+`pip install requirements.txt`
---
+++ main.py
@@ -0,0 +1,15 @@
+import dotenv
+import time
+import threading
+from awsiot import mqqtt_connection_builder
+
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
+
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+
+if __name__ == '__main__':
+    print("Hello World")
